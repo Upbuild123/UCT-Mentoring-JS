2dcd5476c75ab30da38a90bc7d5e6d86696aa497
Michael Sloyer <michaelsloyer@MacBook-Pro-8.local>
2026-06-08 21:37:06 +0900

    feat: shared CSS and JS API wrapper

---
+++ public/js/api.js
@@ -0,0 +1,22 @@
+async function apiFetch(url, options = {}) {
+  const token = sessionStorage.getItem('adminToken');
+  const headers = { 'Content-Type': 'application/json', ...(options.headers || {}) };
+  if (token) headers['Authorization'] = `Bearer ${token}`;
+  if (options.body && typeof options.body !== 'string') {
+    options.body = JSON.stringify(options.body);
+  }
+  const res = await fetch(url, { ...options, headers });
+  if (!res.ok) {
+    const err = await res.json().catch(() => ({ error: res.statusText }));
+    throw new Error(err.error || res.statusText);
+  }
+  return res.json();
+}
+
+function showBanner(container, message, type = 'error') {
+  const div = document.createElement('div');
+  div.className = `alert alert-${type}`;
+  div.textContent = message;
+  container.prepend(div);
+  setTimeout(() => div.remove(), 8000);
+}
